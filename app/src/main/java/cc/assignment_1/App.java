881006d90c1bb48bbb4841c932e174e89f0e26ca
Daniel Egger <dan.egg@outlook.com>
2023-03-18 15:53:59 +0100

    Outline program flow

--- app/src/main/java/cc/assignment_1/App.java
+++ app/src/main/java/cc/assignment_1/App.java
@@ -10,5 +10,36 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+
+        // Check commamnd line arguments (valid url, depth is number, ...):
+
+        // Main recursive function:
+
+            // Get document contents (GET request):
+
+            // Parse file to get <a> and <h*> tags:
+
+            // Write summary of <h*> tags to StringBuilder:
+
+                // Concatenate all <h*> tags:
+
+                // Translate <h*> tags using DeepL:
+
+                    // Send API call to "api-free.deepl.com":
+
+                    // Read response:
+
+            // For each <a> tag:
+
+                // Send HEAD request to check for broken links:
+
+                    // Write broken link (<s>...</s>) and return:
+
+                // If not broken:
+
+                    // Write link to StringBuilder:
+
+                    // Write output of recursive call to StringBuilder (include current indentation level):
+
     }
 }
