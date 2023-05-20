3f0591c5ff338b7f4194d9ee1c4c2b35566d1e6a
Laura Randl <gollumeye@outlook.de>
2023-05-20 21:57:55 +0200

    Refactored ErrorLoggerTest

--- app/src/test/java/cc/assignment/ErrorLoggerTest.java
+++ app/src/test/java/cc/assignment/ErrorLoggerTest.java
@@ -8,23 +8,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ErrorLoggerTest {
     private ErrorLogger errorLogger;
+    String errorLog;
 
     @BeforeEach
     void setUp() {
         errorLogger = ErrorLogger.getErrorLogger();
         errorLogger.addNewErrorMessage("This is a test error message", "SomeClass", "SomeExceptionClass");
         errorLogger.addNewErrorMessage("Another error message", "AnotherClass", "AnotherExceptionClass");
-    }
 
-    String getErrorLogForFormatErrorsToStringTest() {
-        return """
+        errorLog = """
                 Error (SomeExceptionClass) in class SomeClass: This is a test error message
                 Error (AnotherExceptionClass) in class AnotherClass: Another error message
                 """;
     }
 
     @Test
-    void formatErrorsToStringTest() {
-        assertEquals(getErrorLogForFormatErrorsToStringTest(), errorLogger.getErrorLog());
+    void getErrorLogTest() {
+        assertEquals(errorLog, errorLogger.getErrorLog());
     }
 }
